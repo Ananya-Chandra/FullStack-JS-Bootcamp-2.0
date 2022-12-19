3584281bd6a490d461eb171f7449ccf0d327fd50
Ananya-Chandra <107414337+Ananya-Chandra@users.noreply.github.com>
2022-12-19 18:41:47 +0530

    Add files via upload

---
+++ Java Script/10 DEC/sat_script.js
@@ -0,0 +1,122 @@
+// //premitive values or Premitive data type => one single value
+
+// //Number
+// 7;
+// 2.5;
+
+// //string
+// "hi"
+// 'hi'
+
+// //boolean
+// true
+// false
+
+// //empty values
+// null;
+// undefined;
+
+
+
+
+
+// //Non premitive data type => more than one value
+
+// //Array and objects
+
+// //array
+// [1,6,"hi",true]
+
+// //objects
+// {
+//     key:value
+// }
+
+
+
+
+// //variables
+
+// let x = 25; //can change later
+// const z = 90; // constant
+// var y = 56; //don't use
+
+
+// let firstname = 'Anurag';
+// let lastname = 'Tiwari';
+// const mobileno = 55668899;
+
+// console.log('I am', firstname);
+// console.log('last name is' + lastname);
+// console.log(mobileno);
+
+
+
+
+// //Template litera
+// console.log(`My details are ${firstname} ${lastname} ${mobileno}`)
+
+
+
+
+//Operator
+//Assignment 
+
+// let x = 10;
+// let y = 10;
+
+//arithmatic
+// +, -, *, /, % =>remainder
+
+// let z = x+y;
+// console.log(z);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+
+
+//comparison 
+// let abc = 10;
+// let xyz = 20;
+
+// console.log(abc > xyz);
+// console.log(abc >= xyz);
+
+
+//== & ===
+// == > compares values only not datatypes
+// === > compares values & datatypes both
+
+
+// let y;
+// console.log(y);
+
+
+//in case of const we have to declare value at the same time
+//and we can't change the value later
+// const accno = 565655;
+
+
+
+//conditions or decisions
+// let age = 19;
+
+// if (age>=18){
+//     console.log('you are allowed');
+// } else{
+//     console.log('you are not allowed');
+// }
+
+let standard = 3;
+
+if(standard == 1){
+    console.log("Room one");
+} else if( standard==2){
+    console.log("Room two");
+} else if(standard == 3){
+    console.log("Room three");
+} else{
+    console.log("another")
+}
+
