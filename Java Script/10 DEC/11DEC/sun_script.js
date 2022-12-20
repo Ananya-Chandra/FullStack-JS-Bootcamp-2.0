c03ef2140d2848d27f6fc55c583ec565f43524ba
Ananya-Chandra <107414337+Ananya-Chandra@users.noreply.github.com>
2022-12-20 18:48:37 +0530

    Add files via upload

---
+++ Java Script/10 DEC/11DEC/sun_script.js
@@ -0,0 +1,123 @@
+//Conditions
+
+//IF ELSE
+//switch cases
+//Ternary OP / ternary conditions
+
+
+
+//switch case
+
+// let user = 'Admin';
+
+// switch(user){
+//     case 'Admin':
+//         console.log('Redirect to admin');
+//         break;
+//     case 'Mentor':
+//         console.log('Redirect to mentor');
+//         break;
+//     default:
+//         console.log('I am user');
+
+// }
+
+
+
+//ternary condition
+
+// condition ? True
+//            : False
+
+// let userlogin = true;
+// userlogin ? console.log('logout') : console.log('login');
+
+
+
+//array
+// name = ["tina", "mina", "rina", "nina"];
+// console.log(name);
+// console.log(name[2]);
+
+
+//object
+
+
+
+//Date and math
+// const now = new Date();
+// console.log(now)
+//console.log(now.toString());
+
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDay());
+
+// console.log(now.getTime());
+
+// console.log(now.getHours());
+// console.log(now.getMinutes());
+// console.log(now.getSeconds());
+
+
+
+//Math
+// const PI = Math.PI;
+// console.log(PI);
+
+// console.log(Math.round(PI));
+// console.log(Math.round(9.8));
+
+// console.log(Math.floor(9.5));  //lower value 9
+// console.log(Math.cell(9.5));   //upper value 10
+
+// console.log(Math.min(10,4,1,2))  //1
+// console.log(Math.min(10,4,1,2))  //10
+
+// console.log(Math.random())  //any value from (0-1)
+// console.log(Math.random() *10) // to get (0-10)
+// console.log(Math.round(Math.random()*10))   //to get round value
+
+// console.log(Math.pow(3,3)) //power
+
+// console.log(Math.log(2)) //log
+
+// console.log(Math.sin(90));
+
+
+
+
+
+//Loops
+//do while, while, for
+//do while - 1st it will execute once then it will check condition
+
+// let i = 6;
+// do{
+//     console.log('Value of i is : ', i);
+//     i ++;
+// }while (i <= 5);
+// //only print 6 then will not run cause condition is not applied
+
+
+// let j = 1;
+
+
+// while (j <= 5){
+//     console.log('Valuw of J is ', j);
+//     j ++;
+// }
+
+
+
+// for (let i = 0; i<= 5; i++){
+//     console.log('Value of i is ', i)
+// }
+
+
+
+
+//template literal
+// for (let i = 0; i <= 10; i++){
+//     console.log(`${i} * ${i} = ${i*i}`);
+// }
