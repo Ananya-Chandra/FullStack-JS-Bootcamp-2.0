70e28263a31b0710474ead524e3d8bdee54d1c64
Ananya-Chandra <107414337+Ananya-Chandra@users.noreply.github.com>
2022-12-21 22:37:29 +0530

    Add files via upload

---
+++ Java Script/17 DEC/script.js
@@ -0,0 +1,39 @@
+//Array methods
+
+
+
+let names = ['Hitesh', 'Anurag', 'Surya', 'Anirudh', 'Bipul'];
+// console.log(names);
+
+// console.log(names.length);
+
+// console.log(names[3]);
+
+// console.log(names[names.length-1]);
+
+// names[4] = 'Vinay';
+// console.log(names)
+
+
+//Another way to declare a array
+// let names = new Array('Name1', 'Name2');
+// console.log(names);
+
+
+//push - insert value at end
+// names.push('vyom');
+// console.log(names);
+
+
+//slice
+// console.log(names.slice(1,3)) //0, 1, 2
+
+
+//splice
+// let fruits = ['A','B','C','D'];
+// fruits.splice(2, 0 , 'k','L');
+// console.log(fruits)
+
+// let fruits = ['A','B','C','D'];
+// fruits.splice(2, 1 , 'k','L');
+// console.log(fruits)  //removes 1no after 2 index, no delete when 0 as before
