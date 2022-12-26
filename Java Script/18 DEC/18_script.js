d86244a756ab6a88970eb3ec06137628d1b59359
Ananya-Chandra <107414337+Ananya-Chandra@users.noreply.github.com>
2022-12-26 20:27:34 +0530

    Add files via upload

---
+++ Java Script/18 DEC/18_script.js
@@ -0,0 +1,62 @@
+// Function
+
+//Normal function
+
+// function sum(){
+//     let num1 = 16;
+//     let num2 = 25;
+//     console.log('addition is: ', num1+num2)
+// }
+// sum()
+
+
+
+//Function with parameters
+
+// function sum (val1, val2){
+//     console.log('Addition is: ', val1+val2)
+// }
+// sum(52, 25);
+// sum(56, 11);
+
+
+
+//Function with return type
+
+// function sum(val1, val2){
+//     let result = val1 + val2;
+//     return result;
+// }
+
+// let add = sum(10, 25);
+// console.log(add);
+
+// let add1 = sum(41, 56);
+// console.log(add1);
+
+
+
+//string function
+
+// function URL(url, domain){
+//     let con = 'https://';
+//     let result = con + url + domain;
+//     return result;
+// }
+
+// let anuragsite = URL('anuragtiwari', '.me');
+// console.log(anuragsite)
+
+
+//passing more values than arguments
+
+// function sum(arr){
+//     let sum = 0;
+//     for (let i =0; i < arr.length; i++){
+//         sum = sum + arr[i];
+//     }
+//     return sum;
+// }
+// let sumarray = [1,2,3,4,5];
+// let arrresult = sum(sumarray);
+// console.log(arrresult);
