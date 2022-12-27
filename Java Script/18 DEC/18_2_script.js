557ac221864909e355b7e2ce55c8d00a853588c4
Ananya-Chandra <107414337+Ananya-Chandra@users.noreply.github.com>
2022-12-27 10:51:18 +0530

    Add files via upload

---
+++ Java Script/18 DEC/18_2_script.js
@@ -0,0 +1,66 @@
+//function continue
+
+
+//no arguments passed
+
+// function nihal(){
+//     console.log(arguments);
+//     let sum = 0;
+//     for (let i =0; i< arguments.length; i++){
+//         sum = sum + arguments[i];
+//     }
+//     return sum;
+// }
+
+// console.log(nihal(7,8,9,10,11,12,13));
+
+
+
+//arrow function
+
+// let jaimin = () => {
+//     console.log('I am god');
+// };
+// jaimin()
+
+
+
+//arrow fun with arguments
+
+// let jaimin = (var1, var2) => {
+//     console.log(var1, var2);
+// };
+// jaimin(1, 2);
+
+
+
+//object
+
+// let username = {
+//     firstname: 'anurag',
+//     lastname: 'tiwari',
+//     role: 'admin',
+//     loginCount: 25,
+// };
+
+// console.log(username.role);
+
+// username.loginCount = 61
+// console.log(username);
+
+
+
+//for in
+
+// let username = {
+//     firstname: 'anurag',
+//     lastname: 'tiwari',
+//     role: 'admin',
+//     loginCount: 25,
+// };
+
+// for (let x in username){
+//     console.log(x)
+
+//     console.log(username[x])
+// }
